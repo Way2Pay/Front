30244e91f226c5f2a4f39a7cdfad2c6c1458eb38
magus96 <anirudh5462@gmail.com>
2023-10-14 22:53:35 +0530

    Added deploy page for goerli

---
+++ pages/deploy.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { NextPage } from "next";
+import DeployWelcome from "./components/deploy/Deploy";
+const Deploy: NextPage = () => {
+  return (
+    <>
+      <DeployWelcome />
+    </>
+  );
+};
+
+export default Deploy;
