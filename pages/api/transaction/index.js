bee09085aeb7132e2da5748012d98bdb28509f3a
Dhruv Gupta <dhruv035@gmail.com>
2023-10-21 01:29:39 +0530

    Revert

--- pages/api/transaction/index.js
+++ pages/api/transaction/index.js
@@ -1,19 +1,3 @@
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
 
 import clientPromise from "../../../db/database";
 import {
@@ -82,5 +66,3 @@ export default async function handler(request, response) {
     return response.status(400).json({ message: "Incorrect Method" });
   }
 }
-
-module.exports =allowCors(handler)
