fad9284732f93f8087a4a7e1bccc4a6d71c2b827
Dhruv Gupta <dhruv035@gmail.com>
2023-10-20 23:31:13 +0530

    New Push

--- pages/api/transaction/[transactionId].js
+++ pages/api/transaction/[transactionId].js
@@ -19,11 +19,11 @@ export default async function handler(request, response) {
         {
           await db.collection("Transactions").updateOne({_id:id},{$set:{status:"Expired"}},(err,res)=>{
             if(err) throw err
-            return response.status(301).headers({"Access-Control-Allow-Origin": "*"}).json({message:"Transaction has expired"})
+            return response.status(301).json({message:"Transaction has expired"})
           })
         }
         console.log("DATA", res);
-        return response.status(200).headers({"Access-Control-Allow-Origin": "*"}).json({ data: res });
+        return response.status(200).json({ data: res });
       });
   }
 }
